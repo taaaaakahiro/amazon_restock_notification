d56dc1a9a1b0483ea615545a7a7ac5d09fe13db1
YujiTanioka <kanikani21kanikani@gmail.com>
2022-03-08 21:56:05 +0900

    requestなど記載！

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,7 @@
 # インストールした discord.py を読み込む
 import discord
-
+import requests
+from bs4 import BeautifulSoup
 # TOKEN
 TOKEN = 'OTUwNzMwNzAyNTYxODczOTMx.YidK9w.mDA7lovwrRhQ5rzRCJbH9-Dctbw'
 
@@ -20,8 +21,12 @@ async def on_message(message):
     if message.author.bot:
         return
     # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
+    if message.content == '/amazon_now':
+        uri = "https://www.amazon.co.jp/gp/offer-listing/4873117380/"
+        ret = requests.get(uri)
         await message.channel.send('にゃーん')
-
+        soup = BeautifulSoup(ret.content,"lxml")
+        soup.find('div', {'class':'a-row a-spacing-mini olpOffer'})
+        
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)   
