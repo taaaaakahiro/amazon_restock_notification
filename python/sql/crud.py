ebca85f9b9ef70793728b6c03f5274ecc787926c
fiks <fiks@fiksnoMacBook-puro.local>
2022-03-14 21:54:59 +0900

    push

--- python/sql/crud.py
+++ python/sql/crud.py
@@ -1,15 +1,30 @@
-import psycopg2
+import mysql.connector as mydb
 
-def get_connection():
-  username = 'root'
-  password = 'admin'
-  database = 'amazonRestockBot'
-  hostname = 'db'
-  port = 5432
-  dburl = f'postgresql://{username}:{password}@{hostname}:{port}/{database}'
-  conn = psycopg2.connect(dburl)
-  print(1111)
-  return conn
+conn = mydb.connect(
+  host='db',
+  user='admin',
+  password='admin',
+  port='3306',
+  database='amazonRestockBot',
+)
+
+# コネクションが切れた時に再接続してくれるよう設定
+conn.ping(reconnect=True)
+
+# 接続できているかどうか確認
+print(conn.is_connected())
+
+
+# def get_connection():
+#   username = 'root'
+#   password = 'admin'
+#   database = 'amazonRestockBot'
+#   hostname = 'db'
+#   port = 5432
+#   dburl = f'postgresql://{username}:{password}@{hostname}:{port}/{database}'
+#   conn = psycopg2.connect(dburl)
+#   print(1111)
+  # return conn
 
 # id, name, priceを持つテーブルを（すでにあればいったん消してから）作成
 # table = 'test_table'
