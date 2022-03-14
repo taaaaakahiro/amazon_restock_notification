e854fbdc240971104dd9bfe549fb7f60c2ede5ca
fiks <fiks@fiksnoMacBook-puro.local>
2022-03-14 22:49:14 +0900

    接続&テーブル作成

--- python/sql/crud.py
+++ python/sql/crud.py
@@ -1,40 +1,34 @@
 import mysql.connector as mydb
 
-conn = mydb.connect(
-  host='db',
-  user='admin',
-  password='admin',
-  port='3306',
-  database='amazonRestockBot',
-)
+def connect():
+  conn = mydb.connect(
+    host='db',
+    user='admin',
+    password='admin',
+    port='3306',
+    database='amazonRestockBot',
+  )
+  # コネクションが切れた時に再接続してくれるよう設定
+  conn.ping(reconnect=True)
 
-# コネクションが切れた時に再接続してくれるよう設定
-conn.ping(reconnect=True)
+  # 接続できているかどうか確認
+  print(conn.is_connected())
 
-# 接続できているかどうか確認
-print(conn.is_connected())
+  # DB操作用にカーソルを作成
+  cur = conn.cursor()
+  cur.execute("DROP TABLE IF EXISTS merchandise")
+  # id, name, priceを持つテーブルを（すでにあればいったん消してから）作成
+  sql = '''
+    CREATE TABLE merchandise(
+       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+       asin_code VARCHAR(50) NULL,
+       price INT(50) NULL,
+       created_at DATE NULL,
+       deleted_at DATE NUll
+    )'''
+  cur.execute(sql)
+    
 
+connect()
 
-# def get_connection():
-#   username = 'root'
-#   password = 'admin'
-#   database = 'amazonRestockBot'
-#   hostname = 'db'
-#   port = 5432
-#   dburl = f'postgresql://{username}:{password}@{hostname}:{port}/{database}'
-#   conn = psycopg2.connect(dburl)
-#   print(1111)
-  # return conn
-
-# id, name, priceを持つテーブルを（すでにあればいったん消してから）作成
-# table = 'test_table'
-# cur.execute("DROP TABLE IF EXISTS `%s`;", table)
-# cur.execute(
-#     """
-#     CREATE TABLE IF NOT EXISTS `%s` (
-#     `id` int auto_increment primary key,
-#     `name` varchar(50) not null,
-#     `price` int(11) not null
-#     ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci
-#     """, table)
 
