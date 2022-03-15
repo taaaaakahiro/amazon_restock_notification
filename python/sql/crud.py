aa1f6ed62802f00c7eac40c78997d332eb26f0ca
YujiTanioka <kanikani21kanikani@gmail.com>
2022-03-15 21:06:48 +0900

    動作確認前 表示、追加、削除処理

--- python/sql/crud.py
+++ python/sql/crud.py
@@ -1,34 +1,81 @@
 import mysql.connector as mydb
+import datetime
 
 def connect():
-  conn = mydb.connect(
-    host='db',
-    user='admin',
-    password='admin',
-    port='3306',
-    database='amazonRestockBot',
-  )
-  # コネクションが切れた時に再接続してくれるよう設定
-  conn.ping(reconnect=True)
+    conn = mydb.connect(
+        host='db',
+        user='admin',
+        password='admin',
+        port='3306',
+        database='amazonRestockBot',
+    )
+    # コネクションが切れた時に再接続してくれるよう設定
+    conn.ping(reconnect=True)
 
-  # 接続できているかどうか確認
-  print(conn.is_connected())
+    # 接続できているかどうか確認
+    print(conn.is_connected())
 
-  # DB操作用にカーソルを作成
-  cur = conn.cursor()
-  cur.execute("DROP TABLE IF EXISTS merchandise")
-  # テーブルを（すでにあればいったん消してから）作成
-  sql = '''
+    # DB操作用にカーソルを作成
+    cur = conn.cursor()
+    cur.execute("DROP TABLE IF EXISTS merchandise")
+    # テーブルを（すでにあればいったん消してから）作成
+    sql = '''
     CREATE TABLE merchandise(
-       id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-       asin_code VARCHAR(50) NULL,
-       price INT(50) NULL,
-       created_at DATE NULL,
-       deleted_at DATE NUll
+      id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+      asin_code VARCHAR(50) NULL,
+      price INT(50) NULL,
+      created_at DATE NULL,
+      deleted_at DATE NUll
     )'''
-  cur.execute(sql)
-    
-
-connect()
+    cur.execute(sql)
 
 
+def tmp_connect():
+    conn = mydb.connect(
+        host='db',
+        user='admin',
+        password='admin',
+        port='3306',
+        database='amazonRestockBot',
+    )
+    return conn
+
+def get_merchandise():
+    conn = tmp_connect()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            'SELECT * FROM merchandise WHERE NOT NULL deleted_at'
+        )
+        data = cur.fetchall()
+        return data
+
+    except IndexError:
+        return False
+
+
+def add_merchandise(asin_code, price):
+    conn = tmp_connect()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            'INSERT INTO merchandise (asin_code, price ) VALUES (%s, %s)',
+            (asin_code, price)
+        )
+        return True
+
+    except IndexError:
+        return False
+
+def del_merchandise(id):
+    conn = tmp_connect()
+    cur = conn.cursor()
+    try:
+        cur.execute(
+            'UPDATE merchandise (deleted_at) VALUES (%s, %s)',
+            (datetime.datetime.now())
+        )
+        return True
+
+    except IndexError:
+        return False
